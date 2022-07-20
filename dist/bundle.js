502a977effe034b1c6dcd5fa92c53d2f0b0a4a94
ShyamMahajan <shyammahajan2512@gmail.com>
2022-07-20 20:58:46 +0530

    typescript dev setup

---
+++ dist/bundle.js
@@ -0,0 +1,12 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+/*!********************!*\
+  !*** ./src/app.ts ***!
+  \********************/
+
+console.log("test");
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7QUFBQSxPQUFPLENBQUMsR0FBRyxDQUFDLE1BQU0sQ0FBQyIsInNvdXJjZXMiOlsid2VicGFjazovL21hbmdvYXBwLWNoYWxsZW5nZS8uL3NyYy9hcHAudHMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc29sZS5sb2coXCJ0ZXN0XCIpIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
