2ac7f1da0ee36aa46613f4b80d89ea357a06e262
ShyamMahajan <shyammahajan2512@gmail.com>
2022-07-20 20:46:20 +0530

    create ticket, update and render slot

--- src/App.js
+++ src/App.js
@@ -30,28 +30,34 @@ class ParkingSlots {
     }
 
     render(){
-        if(!this.slots.length){
-
+        const parkingSlots = document.querySelector(".parking-slots");
+        if(!this.slots.length && parkingSlots){
+            const divMain = document.createElement("div")
+            divMain.classList.add("no-slots")
+            divMain.textContent = "no slots"
+            parkingSlots.appendChild(divMain)
+        }else{
+            const noSlot = document.querySelector(".no-slots")
+            if(noSlot) noSlot.remove()
         }
     }
 
     renderSlot(slot){
-
         const parkingSlots = document.querySelector(".parking-slots");
-
         if(parkingSlots){
                 const divMain = document.createElement("div")
                 divMain.classList.add("parking-slot")
+                divMain.classList.add(`parking-slot--${slot.number}`)
                 
                 const slotNum = document.createElement("h1")
                 slotNum.classList.add("parking-slot__number")
                 slotNum.textContent = slot.number
                 const slotCar = document.createElement("div")
                 slotCar.classList.add("parking-slot__car")
-                slotCar.textContent = slot.car_number
+                slotCar.textContent = slot.car_color || ""
                 const slotReg = document.createElement("div")
                 slotReg.classList.add("parking-slot__reg-number")
-                slotReg.textContent = slot.regNum
+                slotReg.textContent = slot.regNum || ""
 
                 divMain.appendChild(slotNum)
                 divMain.appendChild(slotCar)
@@ -61,8 +67,20 @@ class ParkingSlots {
         }
     }
 
+    updateSlot(slot){
+        console.log("fsdf",slot)
+        const parkingSlots = document.querySelector(".parking-slots");
+        const Parkslot = parkingSlots.querySelector(`.parking-slot--${slot.number}`)
+        Parkslot.style['background-color'] = slot.car_color
+        const slot_car = Parkslot.querySelector(".parking-slot__car")
+        if(slot_car) slot_car.textContent = slot.ticket_number
+        const slot_reg_no = Parkslot.querySelector(".parking-slot__reg-number")
+        if(slot_reg_no) slot_reg_no.textContent = slot.car_number
+    }
+
     addSlot(){
         this.slots.push(new ParkingSlot(this.slots.length+1));
+        this.render()
         this.renderSlot(this.slots[this.slots.length-1])
         return this.slots;
     }
@@ -73,12 +91,12 @@ class ParkingSlots {
     }
 
     park(slot_no, car_color, car_reg_number, tix){
-        console.log("this data", slot_no, car_color, car_reg_number, tix)
         const slot = this.slots.find(Pslot => Pslot.number === slot_no)
         slot.isFree = false;
         slot.car_color= car_color
         slot.car_number = car_reg_number
         slot.ticket_number = tix
+        this.updateSlot(slot)
         return slot;
     }
 }
@@ -110,8 +128,13 @@ class Cars {
     }
 
     addCar(color, reg_no){
-        this.cars.push(new Car(color, reg_no))
-        return this.cars[this.cars.length - 1]
+        const car = this.cars.find(item => item.reg_no === reg_no)
+        if(car){
+            return car
+        }else{
+            this.cars.push(new Car(color, reg_no))
+            return this.cars[this.cars.length - 1]
+        }
     }
 }
 const cars = Cars.getInstance()
@@ -147,7 +170,7 @@ class Tickets {
         const slot_no = parkingSlots.getAvailableSlot()
         const ticket = this.addTicket(slot_no.number, car.reg_no)
         const slot = parkingSlots.park(slot_no.number, car.color, car.reg_no, ticket.number)
-        console.log("ticket",slot)
+        console.log(slot)
 
     }
 
@@ -161,7 +184,7 @@ class Tickets {
     }
 
     addTicket(slot_no, car_reg_number){
-        this.tickets.push(new Ticket(slot_no, this.tickets.length + 1, car_reg_number))
+        this.tickets.push(new Ticket(slot_no, `TI_#${this.tickets.length + 1}`, car_reg_number))
         return this.tickets[this.tickets.length - 1];
     }
 }
